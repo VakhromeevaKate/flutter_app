2bf062990b19a5557c4ab97dc811d52b8fdd333e
evakhromeeva <vakhromeevae@gmail.com>
2023-06-18 19:09:11 +0300

    [TEST-1] (chore) change theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Text('hello, world!'),
-));
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Colors.deepOrangeAccent,
+          secondary: Colors.orange,
+        ),
+        textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.purple)),
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Hello, world!')),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () { print('Button pressed!'); },
+        ),
+        body: const Center(
+          child: Text('HELLO WORLD!', style: TextStyle(
+            fontSize: 20,
+            fontFamily: 'Times New Roman',
+            color: Colors.red,
+          )),
+        ),
+      )
+    );
+  }
+}
